6b0eb0a595269f86120cc79f7f2e34c4674d179a
Ochkalov <ochkalov@gmail.com>
2016-03-17 12:52:08 +0200

    Готово

---
+++ goitHW/js_1-2/js/js1.js
@@ -0,0 +1,14 @@
+function pow(a, b){
+  var result;
+
+    result = Math.pow(a, b);
+        return result;
+}
+    var a = prompt('Nomber=', '');
+    var b = prompt('Exponent=', '');
+
+console.log( pow(a, b) ); 
+
+//************************************************
+
+
