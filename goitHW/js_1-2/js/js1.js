691ac62a7f77f066e6d26c8c0c23846f67fe5bdc
Ochkalov <ochkalov@gmail.com>
2016-03-21 11:22:48 +0200

    Проверка Целые числа

--- goitHW/js_1-2/js/js1.js
+++ goitHW/js_1-2/js/js1.js
@@ -5,8 +5,16 @@ function pow(a, b){
         return result;
 }
     var a = prompt('Nomber=', '');
-    var b = prompt('Exponent=', '');
-
+        if (Math.round(a) == a) {
+//            console.log(a);
+        var b = prompt('Exponent=', '');
+            if (Math.round(b) != b) {
+                alert('Введите целое число')
+             };
+            
+        } else {
+            alert('Введите целое число')
+        }
 console.log( pow(a, b) ); 
 
 //************************************************
