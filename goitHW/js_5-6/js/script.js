6b8b3abacf886f6e7b11f683e8fd1e49659657fa
Ochkalov <ochkalov@gmail.com>
2016-03-28 15:21:37 +0300

    Ступор не правельно милисек.

---
+++ goitHW/js_5-6/js/script.js
@@ -0,0 +1,59 @@
+var secondsRemaining;
+var intervalHandle;
+
+startCountUp()
+
+function tick() {
+    // grab the h1
+//    var timeDisplay = document.getElementsByClassName("time");
+    var timeDisplay = document.getElementById("time");
+    var msDisplay = document.getElementById('time-ms');
+  //   console.log('timeDisplay', timeDisplay);
+//     console.log('msDisplay', msDisplay);
+    
+    // turn seconds into hh:mm:ss
+    var miSec = setInterval(0);
+    var ms = miSec;
+    var sec = Math.round(miSec / 1000);
+    var min = Math.round(sec / 60);
+ //    console.log('secondsRemaining', secondsRemaining);
+    var hh  = Math.round(min * 60);
+ //   console.log('sec', sec);
+    
+  //  miSec++;
+// console.log('miSec', miSec);
+    
+    // add a leading zero (as a string value) if seconds less than 10
+    if (sec < 10) {
+        sec = "0" + sec;
+    }
+    if (min < 10) {
+        min = "0" + min;
+    }
+    if (hh < 10) {
+        hh = "0" + hh;
+    }
+    
+    // concatenate with colon
+    var message = hh+ ":" + min + ":" + sec;
+    var messageMs = ms;
+    // now change the display
+    timeDisplay.innerHTML = message;
+    msDisplay.innerHTML = messageMs;
+        
+    // subtract from seconds remaining
+  //  secondsRemaining++;
+}
+
+function startCountUp() {
+    // get contents of the "minutes" text box
+    // var miSec = 0;
+    
+    // how many seconds?
+  //  secondsRemaining = Math.round(miSec / 1000) ;
+    
+//    console.log('secondsRemaining', secondsRemaining);
+    // every second, call the "tick" function
+    intervalHandle = setInterval(tick, 1000);
+   // intervalHandle = setTimeout(tick, 1000);
+}
