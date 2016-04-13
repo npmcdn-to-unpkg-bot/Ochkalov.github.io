b181d9d614621ca589594ba4a1284fd1f207154c
Ochkalov <ochkalov@gmail.com>
2016-04-13 18:11:36 +0300

    селект + карусель

---
+++ goitHW/js_9-10/js/script-select.js
@@ -0,0 +1,16 @@
+// $(document).ready(function(){
+//     $('.mySelectBoxClass').customSelect();
+//     /* -OR- set a custom class name for the stylable element */
+//     $('.mySelectBoxClass').customSelect({customClass:'myOwnClassName'});
+// });
+
+
+// cusel
+jQuery(document).ready(function(){
+var params = {
+		changedEl: ".lineForm select",
+		visRows: 5,
+		scrollArrows: true
+	}
+	cuSel(params);
+});
