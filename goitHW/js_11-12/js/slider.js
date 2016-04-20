f52d9d83169b6b523d6784669b809142d0e1bfb3
Ochkalov <ochkalov@gmail.com>
2016-04-20 16:47:32 +0300

    сделал плагин

--- goitHW/js_11-12/js/slider.js
+++ goitHW/js_11-12/js/slider.js
@@ -1,29 +1,34 @@
-$(document).ready(function() {
-    var leftUIEl = $('.carousel-arrow-left');
-    var rightUIEl = $('.carousel-arrow-right');
-    var elementsList = $('.carousel-list');
-    var elementsWiev = 3;
+(function( $ ) {
+    $.fn.myPlugin = function() {
 
-    var pixelsOffset = 210;
-    var currentLeftValue = 0;
+        var leftUIEl = $('.carousel-arrow-left');
+        var rightUIEl = $('.carousel-arrow-right');
+        var elementsList = $('.carousel-list');
+        var elementsWiev = 3;
 
-    var elementsCount = elementsList.find('li').length;
+        var pixelsOffset = 210;
+        var currentLeftValue = 0;
 
-    var minimumOffset =  - ((elementsCount - elementsWiev) * pixelsOffset);
-    var maximumOffset = 0;
+        var elementsCount = elementsList.find('li').length;
 
-    leftUIEl.click(function() {
-        if (currentLeftValue != maximumOffset) {
-            currentLeftValue += pixelsOffset;
-            elementsList.animate({ left : currentLeftValue + "px"}, 500);
-        }
-    });
+        var minimumOffset = -((elementsCount - elementsWiev) * pixelsOffset);
+        var maximumOffset = 0;
 
-    rightUIEl.click(function() {
-        if (currentLeftValue != minimumOffset) {
-            currentLeftValue -= pixelsOffset;
-            elementsList.animate({ left : currentLeftValue + "px"}, 500);
-        }
-    });
+        leftUIEl.click(function () {
+            if (currentLeftValue != maximumOffset) {
+                currentLeftValue += pixelsOffset;
+                elementsList.animate({left: currentLeftValue + "px"}, 500);
+            }
+        });
 
-});
+        rightUIEl.click(function () {
+            if (currentLeftValue != minimumOffset) {
+                currentLeftValue -= pixelsOffset;
+                elementsList.animate({left: currentLeftValue + "px"}, 500);
+            }
+        });
+        return this;
+    }
+})(jQuery);
+
+$('.carousel-arrow-left,.carousel-arrow-right').myPlugin();
