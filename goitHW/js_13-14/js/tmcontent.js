aa4a4160b6435f4b1c578846bbd5e6c669c63a4e
Ochkalov <ochkalov@gmail.com>
2016-04-29 17:15:27 +0300

    ГОТОВО задача 3
    На странице должна быть возможность пройти тест. При клике на кнопку
    "Проверить мои результаты" нужно выполнить проверку правильных ответов и
    вывести сообщение об успешном или не успешном прохождении теста. После
    вывода сообщения, обнулить отметки чтоб тест можно было пройти опять

--- goitHW/js_13-14/js/tmcontent.js
+++ goitHW/js_13-14/js/tmcontent.js
@@ -30,24 +30,45 @@ $(function () {
             a1: objAnswLocalGet[0],
             a2: objAnswLocalGet[1],
             a3: objAnswLocalGet[2],
-            a4: objAnswLocalGet[3]
+            a4: objAnswLocalGet[3],
+            a5: objAnswLocalGet[4],
+            correctAnswer: 0, // нумерация ответов с нуля!
+            input1: '<input type=radio class="list-unstyled" name="q' + 1 + '" value="' + 0 + '">',
+            input2: '<input type=radio class="list-unstyled" name="q' + 1 + '" value="' + 1 + '">',
+            input3: '<input type=radio class="list-unstyled" name="q' + 1 + '" value="' + 2 + '">',
+            input4: '<input type=radio class="list-unstyled" name="q' + 1 + '" value="' + 3 + '">',
+            input5: '<input type=radio class="list-unstyled" name="q' + 1 + '" value="' + 4 + '">'
         },
         {
             q: objQweLocalGet[1],
             a1: objAnswLocalGet[0],
             a2: objAnswLocalGet[1],
             a3: objAnswLocalGet[2],
-            a4: objAnswLocalGet[3]
+            a4: objAnswLocalGet[3],
+            a5: objAnswLocalGet[4],
+            correctAnswer: 3,
+            input1: '<input type=radio class="list-unstyled" name="q' + 2 + '" value="' + 0 + '">',
+            input2: '<input type=radio class="list-unstyled" name="q' + 2 + '" value="' + 1 + '">',
+            input3: '<input type=radio class="list-unstyled" name="q' + 2 + '" value="' + 2 + '">',
+            input4: '<input type=radio class="list-unstyled" name="q' + 2 + '" value="' + 3 + '">',
+            input5: '<input type=radio class="list-unstyled" name="q' + 2 + '" value="' + 4 + '">'
         },
         {
             q: objQweLocalGet[2],
             a1: objAnswLocalGet[0],
             a2: objAnswLocalGet[1],
             a3: objAnswLocalGet[2],
-            a4: objAnswLocalGet[3]
+            a4: objAnswLocalGet[3],
+            a5: objAnswLocalGet[4],
+            correctAnswer: 0,
+            input1: '<input type=radio class="list-unstyled" name="q' + 3 + '" value="' + 0 + '">',
+            input2: '<input type=radio class="list-unstyled" name="q' + 3 + '" value="' + 1 + '">',
+            input3: '<input type=radio class="list-unstyled" name="q' + 3 + '" value="' + 2 + '">',
+            input4: '<input type=radio class="list-unstyled" name="q' + 3 + '" value="' + 3 + '">',
+            input5: '<input type=radio class="list-unstyled" name="q' + 3 + '" value="' + 4 + '">'
         }
-        
-        
+
+
     ];
 
 
@@ -56,4 +77,95 @@ $(function () {
     });
 
     $('body').append(content);
-})
+    $( "button" ).on( "click", Score);
+
+
+    var yourAns = new Array;
+    var score = 0;
+
+    $( "input" ).on( "click", Engine);
+
+
+
+    function Engine(question, answer) {
+         // console.log('question',question);
+
+
+        var qwest = (parseNum(question.target.name)) -1;
+       console.log('Вопрос № ',qwest);
+
+         // yourAns[question] = answer;
+        answer = parseInt(question.currentTarget.value);
+        yourAns[qwest] = answer;
+        console.log('Ответа № ',answer);
+
+        console.log('yourAns[]=',yourAns);
+    }
+
+    function parseNum(str){
+        return parseInt(String(str).match(/-?\d+(?:\.\d+)?/g, '') || 0, 10);
+    }
+
+
+
+    function Score(){
+        var answerText = "Результаты:\n";
+
+        console.log('yourAns.length',yourAns.length);
+
+
+
+        for( var i = 0; i < yourAns.length; ++i){
+
+            var num = i+1;
+            answerText = answerText+"\n    Вопрос №"+ num +"";
+
+            if(yourAns[i]!= test[i].correctAnswer){
+                console.log('yourAns[i]',yourAns[i]);
+                answerText = answerText+": НЕ Верно! \n";
+
+                /*answerText = answerText+"\n    Правильный ответ: " +
+                    test[i].answers[test[i].correctAnswer] + "\n";*/
+
+            }
+            else{
+                answerText = answerText+": Верно! \n";
+                ++score;
+            }
+        }
+
+        answerText = answerText +"\nВсего правильных ответов: "+score+"\n";
+
+        alert(answerText);
+        yourAns = [];
+        score = 0;
+        clearForm("quiz");
+    }
+
+
+
+    function clearForm(name) {
+        var f = document.forms[name];
+        for(var i = 0; i < f.elements.length; ++i) {
+            if(f.elements[i].checked)
+                f.elements[i].checked = false;
+        }
+    }
+
+
+    /*for(var q=0; q<test.length; ++q) {
+     var question = test[q];
+     var idx = 1 + q;
+
+     // document.writeln('<li><span class="quest">' + question.text + '</span><br/>');
+     for(var i in question.answers) {
+     document.writeln('<input type=radio name="q' + idx + '" value="' + i +
+     '" onClick="Engine(' + q + ', this.value)">' + '<br/>');
+     }
+     }
+*/
+
+
+
+
+});
