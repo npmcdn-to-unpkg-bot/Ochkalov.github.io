c77707b48b9f823b7d5e51b9e1d337b571cbf7b3
Ochkalov <ochkalov@gmail.com>
2016-05-02 15:42:05 +0300

    Переименование

--- goitHW/js_15-16/js/script.js
+++ goitHW/js_15-16/js/script.js
@@ -1,25 +1,49 @@
-$(document).ready(function() {
-    
-    $("body").keypress(function(e) {
-          if (e.which == 13) {
-              return true;
-          }
-     });
-     
-     // callback function
-    function GoogleCallback (func, data) {
-        window[func](data);
-}
-     // change key!!!
-    $.getJSON("http://ajax.googleapis.com/ajax/services/search/web?v=1.0?key=ABQIAAAACKQaiZJrS0bhr9YARgDqUxQBCBLUIYB7IF2WaNrkYqF0tBovNBQFDtM_KNtb3xQxWff2mI5hipc3lg&q=PHP&callback=GoogleCallback&context=?",
-    function(data){
-        var ul = document.createElement("ul");
-        $.each(data.results, function(i, val){
-                var li = document.createElement("li");
-                li.innerHTML = '<a href="'+val.url+'" title="'+val.url+'" target="_blank">'+val.title+"</a> - "+val.content;                            
-                ul.appendChild(li);
+'use strict';
+/*function GoogleCallback(queryObj, data) {}*/
+function getPage(startNumberArticle) {
+        $.ajax({
+            url: "https://ajax.googleapis.com/ajax/services/search/web?v=1.0&key=ABQIAAAACKQaiZJrS0bhr9YARgDqUxQBCBLUIYB7IF2WaNrkYqF0tBovNBQFDtM_KNtb3xQxWff2mI5hipc3lg&q=" +
+             $('input[name="search-text"]', this).val() + '&start=' + startNumberArticle + '&rsz=8',
+            dataType: 'jsonp',
+            method : 'GET',
+            success: function (data) {
+                if (data.responseStatus == 400) {
+                    alert('Google против таких объемов поиска.');
+                    return true;
+                }
+                
+                if (data.responseStatus != 200) {
+                    alert('Ошибка поиска с кодом: '+ data.responseStatus + ' '+data.responseDetails );
+                    return false;
+                }
+                if (data.responseData.results.length == 0) {
+                    alert('Ничего не найдено');
+                    return true;
+                }
+                $('.result-container__data div').remove();
+                $('.result-container__cursor div').remove();
+                var htmlData = tmpl('googleTemplateData', data.responseData);
+                //                $(htmlData).insertBefore('.result-container__data');
+                $('.result-container__data').append(htmlData);
+                var htmlCursor = tmpl('googleTemplateCursor', data.responseData.cursor);
+                //                $(htmlData).insertBefore('.result-container__cursor');
+                $('.result-container__cursor').append(htmlCursor);
+                var $cursor = data.responseData.cursor;
+            }, 
+            error: function (xhr, textStatus, errorThrown) {
+                alert('Ошибка с кодом '+errorThrown);
+            }
         });
-        $('body').html(ul);
+
+}
+$(function () {
+    $('form').on('submit', function (e) {
+        e.preventDefault();
+        var searchText = $('input[name="search-text"]', this).val();
+        if (searchText.length == 0) {
+            alert('Введите что-нибудь для поиска');
+            return this;
+        }
+        getPage(0);
     });
-     
 });
