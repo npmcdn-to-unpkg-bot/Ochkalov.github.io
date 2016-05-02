255e5d998a1206f301996b2f855c8b71b293d4d6
Ochkalov <ochkalov@gmail.com>
2016-05-02 14:20:34 +0300

    готово 2  ПОЙСК
    Вывести результаты поиска под форму. Дизайн постараться сделать близким
    к дизайну google.com

--- goitHW/js_15-16/js/script.js
+++ goitHW/js_15-16/js/script.js
@@ -5,4 +5,21 @@ $(document).ready(function() {
               return true;
           }
      });
+     
+     // callback function
+    function GoogleCallback (func, data) {
+        window[func](data);
+}
+     // change key!!!
+    $.getJSON("http://ajax.googleapis.com/ajax/services/search/web?v=1.0?key=ABQIAAAACKQaiZJrS0bhr9YARgDqUxQBCBLUIYB7IF2WaNrkYqF0tBovNBQFDtM_KNtb3xQxWff2mI5hipc3lg&q=PHP&callback=GoogleCallback&context=?",
+    function(data){
+        var ul = document.createElement("ul");
+        $.each(data.results, function(i, val){
+                var li = document.createElement("li");
+                li.innerHTML = '<a href="'+val.url+'" title="'+val.url+'" target="_blank">'+val.title+"</a> - "+val.content;                            
+                ul.appendChild(li);
+        });
+        $('body').html(ul);
+    });
+     
 });
