17548ec87a971120cb9d5c85a018357a894d29fb
Ochkalov <ochkalov@gmail.com>
2016-06-22 19:32:04 +0300

    что то похожее

--- Task/Smet/js/js2.js
+++ Task/Smet/js/js2.js
@@ -1,16 +1,21 @@
 function timeLoggingDecorator(func){
+
     var tStart, tEnd;
-    console.log('Name =', func());
+
     tStart = new Date();
+    console.log('Function has started:',  func.name);
+
     func();
     tEnd = new Date();
-    console.log(tEnd.getTime() - tStart.getTime())
+    console.log('Function has finished:', func.name,'.', 'Working time:',tEnd.getTime() - tStart.getTime());
+
+    // console.log(tEnd.getTime() - tStart.getTime());
+
     return tEnd.getTime() - tStart.getTime();
 }
 
-// console.log('Function has started',  getFnName(arguments.callee));
 function fib() {
-    return 100*2000/500;
+    return 100*2000+46842*5474652/500*25498844;
 };
 
 fib = timeLoggingDecorator(fib);
