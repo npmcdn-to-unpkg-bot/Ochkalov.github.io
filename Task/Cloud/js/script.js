aa575acc70fd849e561fec1d0b7584edb237173a
Ochkalov <ochkalov@gmail.com>
2016-07-18 16:01:07 +0300

    random Cloud

---
+++ Task/Cloud/js/script.js
@@ -0,0 +1,30 @@
+// определяем вес слова
+function randomInteger(min, max) {
+    var rand = min - 0.5 + Math.random() * (max - min + 1);
+    rand = Math.round(rand);
+    return rand;
+}
+var rendomMinMax = function () {
+    var rendomM = randomInteger(1, 15);
+    return rendomM;
+};
+var rendom = function () {
+    var weightWords = randomInteger(rendomMinMax(), rendomMinMax());
+    return weightWords;
+};
+
+// создание массива тегов
+var word_list = [
+    {text:"devkeeper", weight: rendom()},
+    {text:"индивидуальный подход", weight: rendom(), html:{title:"My Title", "class":"custom-class"}},
+    {text:"поддержка", weight: rendom()},
+    {text:"ит-консультации", weight: rendom()},
+    {text:"дизайн", weight: rendom()},
+    {text:"теплая атмосфера", weight:rendom()},
+    {text:"креативные идеи", weight:rendom()},
+    {text:"рациональные решения", weight:rendom()},
+    {text:"рфзработка программного обеспечения", weight:rendom()}
+];
+$(function () {
+    $("#example").jQCloud(word_list);
+});
